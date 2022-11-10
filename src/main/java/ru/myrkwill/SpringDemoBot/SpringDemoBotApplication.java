e11c4327a954ab7229c80d38b9328a91814698cf
Mark Nagibin <myrkwill@icloud.com>
2022-11-10 15:08:36 +0500

    Init

---
+++ src/main/java/ru/myrkwill/SpringDemoBot/SpringDemoBotApplication.java
@@ -0,0 +1,13 @@
+package ru.myrkwill.SpringDemoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoBotApplication.class, args);
+	}
+
+}
